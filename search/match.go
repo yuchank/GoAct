fb6d9a573a36e4e6b27853b45e0c0582ff82ae91
kwon.yuchan <kwon.yuchan@nbplus.kr>
2018-11-12 23:29:14 +0900

    interface

--- search/match.go
+++ search/match.go
@@ -1,6 +1,7 @@
 package search
 
 import (
+	"fmt"
 	"log"
 )
 
@@ -14,4 +15,30 @@ type Result struct {
 // to implement a new search type.
 type Matcher interface {
 	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
+
+// Match is launched as a goroutine for each individual feed to run
+// searches concurrently.
+func Match(matcher Matcher, feed *Feed, searchTerm string, results chan <- *Result) {
+	// Perform the search against the specified matcher.
+	searchResults, err := matcher.Search(feed, searchTerm)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Write the results to the channel.
+	for _, result := range searchResults {
+		results <- result
+	}
+}
+
+// Display writes results to the terminal window as they
+// are received by the individual goroutines.
+func Display(results chan *Result) {
+	// The channel blocks until a result is written to the channel.
+	// Once the channel is closed the for loop terminates.
+	for result := range results {
+		fmt.Println("%s:\n%s\n\n", result.Field, result.Content)
+	}
 }
